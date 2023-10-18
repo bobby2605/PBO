6cae979ecb9f7d9ad50f805bae27bc5929bc0c28
bobby2605 <148331929+bobby2605@users.noreply.github.com>
2023-10-18 16:50:17 +0700

    Add files via upload

---
+++ Pertemuan 1/tugas1.py
@@ -0,0 +1,15 @@
+print("menghitung dan keliling persegi panjang")
+
+# variabel
+panjang = 25
+lebar = 5
+
+# Rumus
+luas = panjang * lebar
+kel = (2 * panjang) + ( 2 * lebar)
+
+# Output
+print("panjang : ", panjang)
+print("lebar : ", luas)
+print("luas : ", luas)
+print("keliling :", kel)
